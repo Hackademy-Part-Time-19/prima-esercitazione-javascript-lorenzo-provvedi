9da86f1a4a1d0a2192d794e4f29d9fd17637648e
lory <lory608@gmail.com>
2023-11-09 20:46:44 +0100

    esercizio javascript

---
+++ prima-esercitazione-JS.js
@@ -0,0 +1,98 @@
+var num1= 5
+
+var num2= 7
+
+var tot= 0
+
+var stringa= "Lorenzo"
+
+console.log("adesso" + num1 + "tot" + tot)
+
+num1= num1 + 3
+
+console.log("ora" + num1 + "tot" +tot)
+
+console.log("adesso" + num2 + "tot"+tot)
+
+num1= num1 + 5
+
+console.log("ora" + num2 + "num2tot")
+
+tot= num1 + num2
+
+console.log("totale" + tot)
+
+tot=num1 + num2 + stringa
+
+console.log("totale nuovo" +tot)
+
+var stringa= "L0r3"
+
+console.log("nuovo valore" + stringa)
+
+const PIGRECO =3.14159265359
+
+console.log("pigreco" +PIGRECO +"TOT"+ tot )
+
+var uno=1
+var due=2
+var tre=3
+var quattro=4
+var cinque=5
+
+var somma=0
+
+var media=0
+
+somma=uno+due+tre+quattro+cinque
+
+media=(uno+due+tre+quattro+cinque)/5
+
+console.log("la somma tra i numeri equivale a"+somma+"e la media equivale a"+media)
+
+var annocorrente=2023
+
+var annodinascita=2000
+
+var età=0
+
+var annimancanti=0
+
+var obiettivoanni=100
+
+età=annocorrente-annodinascita
+
+annimancanti=obiettivoanni-età
+
+console.log("hai"+età+"anni e ti mancano"+annimancanti+"anni per compierne 100")
+
+var variabile1=5
+
+var variabile2=15
+
+var Somma=0
+
+var moltiplicazione=0
+
+var sottrazione=0
+
+var divisione=0
+
+var potenza=0
+
+somma=variabile1 + variabile2
+
+moltiplicazione= variabile1 *variabile2
+
+sottrazione=variabile1-variabile2
+
+divisione=variabile1/variabile2
+
+potenza=variabile1**variabile2
+
+console.log("somma"+ somma +"moltiplicazione"+ moltiplicazione +"sottrazione"+ sottrazione +"divisione"+ divisione +"potenza"+potenza)
+
+
+
+
+
